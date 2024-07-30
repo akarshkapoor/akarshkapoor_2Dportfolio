871a12b54aa28fcf129976bbe945e71599eb285e
akarshkapoor <2005707@kiit.ac.in>
2024-07-30 13:45:50 +0530

    first

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `I am a programmer. I work mostly in Java, C++ and Python.
+      I have a keen interest in new and emerging technologies.
+      Here is my <a href="https://github.com/akarshkapoor" target="_blank">Github</a>!`,
+  "cs-degree": `This is my B.Tech degree. I have graduated from Kalinga Institute of Industrial Technology as a Computer Science Engineer.`,
+  "sofa-table": `Hey, I am Akarsh. That's my sofa. I like to relax here. A lot!`,
+  tv: `That's my TV. Information about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1KxwdhUWHbWROWo0XoToelaD264da6q0Q/view?usp=drivesdk" target="_blank">Check it out?</a>`,
+  projects: `I have worked on multiple software projects, such as <a href="https://github.com/IshitaBhagat/decentralized-voting-system">De-centralized Voting System</a> and <a href="https://github.com/Prashant1709/Ocean_Wave_Tracking">Ocean Wave Tracking</a>`,
+  library: `This is my library. I am well versed with developer tools, like Git, GitHub, MySQL, UI Path, Amazon Web Sevices & Microsoft Azure.`,
+  exit: `If you want to exit, just <a href="https://portfolio-mu-black-42.vercel.app">Click here</a>. Bye!!!`,
+};
